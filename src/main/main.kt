fb009986a5f70059c38d170e9f25f2b4599350a9
Dale Louize Almonia <dmalmonia@up.edu.ph>
2025-09-27 23:55:56 +0800

    Implemented complete lexical scanner and basic REPL driver

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,4 +1,12 @@
 fun main() {
     println("Welcome to Man og Language")
+    while (true){
+        val line = readLine()
 
+        val scanner = Scanner(line)
+        val tokens = scanner.scanTokens()
+
+        for(i in tokens)
+            println(i)
+    }
 }
